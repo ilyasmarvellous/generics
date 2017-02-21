89e0102efe1bfa3226b6c6c765c35632962b065a
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-20 22:03:43 -0600

    Add project files.

---
+++ dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>(7);
+            d.Add(1, "Ilyas");
+            d.Add(2, "Teja");
+            d.Add(3, "Umakanth");
+            d.Add(4, "Rajeesh");
+            d.Add(5, "Rohan");
+            d.Add(6, "Bharat");
+            d.Add(7, "Venu");
+            for (int i = 1; i <= d.Count; i++)
+            {
+                Console.WriteLine(d[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
